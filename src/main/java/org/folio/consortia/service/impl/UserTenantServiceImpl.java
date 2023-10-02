ff8cb2e3db376a0a6088b9f4aebbb3917dd97df3
Serhii Nosko <v-snosko@ebsco.com>
2023-10-02 11:38:53 +0300

    [MODCON-101] Fix issue with updating firstName, lastName of user created in member tenant (#118)
    [MODCON-101] Process user type updated from patron to staff/staff to patron

--- src/main/java/org/folio/consortia/service/impl/UserTenantServiceImpl.java
+++ src/main/java/org/folio/consortia/service/impl/UserTenantServiceImpl.java
@@ -209,13 +209,18 @@ public class UserTenantServiceImpl implements UserTenantService {
   }
 
   @Override
-  public void updateShadowUsersFirstAndLastNames(UUID userId) {
+  public void updateShadowUsersFirstAndLastNames(UUID userId, String originalTenantId) {
     List<UserTenantEntity> userTenantEntities = userTenantRepository.getByUserIdAndIsPrimaryFalse(userId);
     if (CollectionUtils.isNotEmpty(userTenantEntities)) {
-      User primaryUser = userService.getById(userId);
-      String firstName = primaryUser.getPersonal().getFirstName();
-      String lastName = primaryUser.getPersonal().getLastName();
-      List<String> tenantIds = userTenantEntities.stream().map(userTenantEntity -> userTenantEntity.getTenant().getId()).toList();
+      String firstName;
+      String lastName;
+      List<String> tenantIds;
+      try (var ignored = new FolioExecutionContextSetter(prepareContextForTenant(originalTenantId, folioModuleMetadata, folioExecutionContext))) {
+        User primaryUser = userService.getById(userId);
+        firstName = primaryUser.getPersonal().getFirstName();
+        lastName = primaryUser.getPersonal().getLastName();
+        tenantIds = userTenantEntities.stream().map(userTenantEntity -> userTenantEntity.getTenant().getId()).toList();
+      }
       log.info("Updating shadow users in all tenants exist in consortia for the user: {}", userId);
       tenantIds.forEach(tenantId -> {
         try (var ignored = new FolioExecutionContextSetter(prepareContextForTenant(tenantId, folioModuleMetadata, folioExecutionContext))) {
