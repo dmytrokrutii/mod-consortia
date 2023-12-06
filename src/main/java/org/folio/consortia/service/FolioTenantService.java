c43cc9bba4816319ebd27a4c02609346bcd39c3f
Azizbek Khushvakov <113523904+azizbekxm@users.noreply.github.com>
2023-12-06 20:47:23 +0500

    [MODCON-123] - Implement mechanism to update system shadow users permissions (#136)
    * [MODCON-123] - Implement mechanism to update system shadow users permissions

--- src/main/java/org/folio/consortia/service/FolioTenantService.java
+++ src/main/java/org/folio/consortia/service/FolioTenantService.java
@@ -1,23 +1,29 @@
 package org.folio.consortia.service;
 
 import java.sql.ResultSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
 
+import lombok.extern.log4j.Log4j2;
 import org.apache.commons.lang3.BooleanUtils;
 import org.apache.commons.lang3.ObjectUtils;
 import org.folio.consortia.config.FolioExecutionContextHelper;
 import org.folio.consortia.config.kafka.KafkaService;
 import org.folio.consortia.domain.dto.CustomField;
 import org.folio.consortia.domain.dto.CustomFieldType;
+import org.folio.consortia.domain.dto.User;
+import org.folio.consortia.exception.ResourceNotFoundException;
 import org.folio.spring.FolioExecutionContext;
+import org.folio.spring.client.PermissionsClient;
 import org.folio.spring.liquibase.FolioSpringLiquibase;
 import org.folio.spring.scope.FolioExecutionContextSetter;
 import org.folio.spring.service.TenantService;
 import org.folio.tenant.domain.dto.TenantAttributes;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
-
-import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 @Service
@@ -30,6 +36,12 @@ public class FolioTenantService extends TenantService {
   private final CustomFieldService customFieldService;
   private final FolioExecutionContext folioExecutionContext;
   private final FolioExecutionContextHelper contextHelper;
+  private final UserService userService;
+  private final PermissionsClient permissionsClient;
+  private final ConsortiaConfigurationService consortiaConfigurationService;
+
+  @Value("${folio.system-user.username}")
+  private String systemUserUsername;
 
   private static final String ORIGINAL_TENANT_ID_NAME = "originalTenantId";
   private static final CustomField ORIGINAL_TENANT_ID_CUSTOM_FIELD = CustomField.builder()
@@ -40,18 +52,18 @@ public class FolioTenantService extends TenantService {
     .visible(false)
     .build();
 
-  public FolioTenantService(JdbcTemplate jdbcTemplate,
-                            KafkaService kafkaService,
-                            FolioExecutionContext context,
-                            FolioSpringLiquibase folioSpringLiquibase,
-                            CustomFieldService customFieldService,
-                            FolioExecutionContext folioExecutionContext,
-                            FolioExecutionContextHelper contextHelper) {
+  public FolioTenantService(JdbcTemplate jdbcTemplate, KafkaService kafkaService, FolioExecutionContext context,
+                            FolioSpringLiquibase folioSpringLiquibase, CustomFieldService customFieldService,
+                            FolioExecutionContext folioExecutionContext, FolioExecutionContextHelper contextHelper,
+                            UserService userService, PermissionsClient permissionsClient, ConsortiaConfigurationService consortiaConfigurationService) {
     super(jdbcTemplate, context, folioSpringLiquibase);
     this.kafkaService = kafkaService;
     this.customFieldService = customFieldService;
     this.folioExecutionContext = folioExecutionContext;
     this.contextHelper = contextHelper;
+    this.userService = userService;
+    this.permissionsClient = permissionsClient;
+    this.consortiaConfigurationService = consortiaConfigurationService;
   }
 
   @Override
@@ -60,6 +72,7 @@ public class FolioTenantService extends TenantService {
       contextHelper.registerTenant();
       kafkaService.createKafkaTopics();
       createOriginalTenantIdCustomField();
+      updateLocalTenantShadowSystemUsers();
     } catch (Exception e) {
       log.error(e.getMessage(), e);
       throw e;
@@ -79,6 +92,50 @@ public class FolioTenantService extends TenantService {
         getSchemaName()
       )
     );
+  }
+
+  /**
+   * In second time installing of module, method looks for system user permission changes
+   * if there is new changes in system user of central tenant,
+   * permission of shadow users will be updated with same ones with central system user in requesting tenant.
+   * This functionality is required by sharing settings because operation from central tenant context use system users,
+   * so each shadow users permissions also should be updated
+   */
+  private void updateLocalTenantShadowSystemUsers() {
+    if (!consortiaConfigurationService.isCentralTenantConfigurationExists()) {
+      log.info("The first time module is being installed, so skipping phase of updating shadow user permission");
+      return;
+    }
+
+    log.debug("updateLocalTenantShadowSystemUsers:: Trying to update shadow user permissions");
+    String requestingTenant = folioExecutionContext.getTenantId();
+    String centralTenantId = consortiaConfigurationService.getCentralTenantId(requestingTenant);
+
+    if (Objects.equals(requestingTenant, centralTenantId)) {
+      log.info("updateLocalTenantShadowSystemUsers:: consortia module is being installed on central tenant, no action required");
+      return;
+    }
+
+    // Fetch system user and its permission lists from central tenant
+    User centralSystemUser;
+    List<String> systemUserPermissionList;
+    try (var ignored = new FolioExecutionContextSetter(contextHelper.getSystemUserFolioExecutionContext(centralTenantId))) {
+      centralSystemUser = userService.getByUsername(systemUserUsername)
+        .orElseThrow(() -> new ResourceNotFoundException("systemUserUsername", systemUserUsername));
+      systemUserPermissionList = permissionsClient.getUserPermissions(centralSystemUser.getId()).getResult();
+    }
+
+    try (var ignored = new FolioExecutionContextSetter(contextHelper.getSystemUserFolioExecutionContext(requestingTenant))) {
+      var shadowCentralSystemUser = userService.getById(UUID.fromString(centralSystemUser.getId()));
+      String shadowSystemUserId = shadowCentralSystemUser.getId();
+      var shadowSystemUserPermissionList = permissionsClient.getUserPermissions(shadowSystemUserId).getResult();
+
+      shadowSystemUserPermissionList.forEach(systemUserPermissionList::remove);
+      systemUserPermissionList.forEach(permission ->
+        permissionsClient.addPermission(shadowSystemUserId, new PermissionsClient.Permission(permission))
+      );
+      log.info("updateLocalTenantShadowSystemUsers:: Permissions assigned to shadow system user: [{}] of tenant: {}", systemUserPermissionList, requestingTenant);
+    }
   }
 
   private void createOriginalTenantIdCustomField() {
